815a46c04c08b9ec856bdb2d042d462f377a67f5
Radityyzz <167454158+Radityyzz@users.noreply.github.com>
2024-04-18 23:20:45 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,16 +10,16 @@ global.APIKeys = {
 	'https://api.zeeoneofc.my.id': 'Your Key',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
-global.packname = namabot
-global.author = namaowner
+global.namabot = "GilzzXD"
+global.namaowner = "Raditya"
+global.packname = GilzzyXD
+global.author = Raditya
 global.myweb = "https://s.id/zeeoneofc"
-global.footer_text = "© " + namabot
+global.footer_text = "© " + GilzzyXD
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
-global.pairingNumber = "" //pairing number to install bot 
+global.owner = ['6285745149901']
+global.pairingNumber = "6285608485173" //pairing number to install bot 
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
 global.prefa = ['-_-']
